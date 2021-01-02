5f3104d3a96ea0c3b0e98801cbe550207deb0675
JackyLN <lenghia1991@gmail.com>
2021-01-02 15:22:03 +0800

    Update n1.py
    add in the total

--- n1.py
+++ n1.py
@@ -1,10 +1,12 @@
 d = d[(d['type'] == '4 ROOM')]
 d = d[(d['remaining_lease'] >= '69 years')]
-d = d[(d['remaining_lease'] <= '89 years 11 months')]
+d = d[(d['remaining_lease'] < '90 years')]
 d = d[(d['price'] >= 400000)]
 d = d[(d['price'] <= 650000)]
 
 filterTown = np.array(['ANG MO KIO','BUKIT MERAH','QUEENSTOWN', 'KALLANG/WHAMPOA', 'JURONG EAST', 'BEDOK'])
+a = d[(np.isin(d['town'],filterTown))]
+
 filterYear = np.arange(2017, 2021)
 
 #data = np.array([])
@@ -18,7 +20,7 @@ for town in filterTown:
         yearTmp = townTmp[np.char.find(townTmp['month'], str(year)) > -1]
         avgPrice = yearTmp['price'].mean()
         data.append([town,year,avgPrice])
-        
-print("There are altogether {} rows of data in this dataset.".format(len(d)))
+
+print("There are altogether {} rows of data in this dataset.".format(len(a)))
 for x in data: 
     print("The median resale flat price for {} in {} is ${:.0f}".format(x[0], x[1], x[2]))
