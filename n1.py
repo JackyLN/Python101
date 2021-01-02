f34a2d48dd709afdf70189ffc72801e9745fa1d4
JackyLN <lenghia1991@gmail.com>
2021-01-02 15:14:02 +0800

    Update n1.py
    auto print

--- n1.py
+++ n1.py
@@ -1,15 +1,24 @@
-f = "data/resale-flat-prices-based-on-registration-date-from-jan-2017-onwards.csv"
-d = np.genfromtxt(f,delimiter=",", skip_header=1, 
-               dtype=[('month', 'U7'),('town', 'U30'),('type', 'U30'),('block', 'i4'),('street', 'U50'),('storey', 'U30'),
-                      ('area', 'i4'), ('flat', 'U30'), ('lease', 'i4'), ('remaining_lease', 'U30'), ('price', 'i8')])
-
 d = d[(d['type'] == '4 ROOM')]
 d = d[(d['remaining_lease'] >= '69 years')]
 d = d[(d['remaining_lease'] <= '89 years 11 months')]
-d = d[(d['price'] >= int('400000'))]
-d = d[(d['price'] <= int('650000'))]
+d = d[(d['price'] >= 400000)]
+d = d[(d['price'] <= 650000)]
 
-filterTown = np.array(['ANG MO KIO','BEDOK','SENGKANG', 'BUKIT BATOK'])
-d  = d[(np.isin(d['town'],filterTown))]
+filterTown = np.array(['ANG MO KIO','BUKIT MERAH','QUEENSTOWN', 'KALLANG/WHAMPOA', 'JURONG EAST', 'BEDOK'])
+filterYear = np.arange(2017, 2021)
 
+#data = np.array([])
+data = []
+
+for town in filterTown:
+
+    townTmp = d[np.char.find(d['town'], str(town))> -1]
+
+    for year in filterYear:
+        yearTmp = townTmp[np.char.find(townTmp['month'], str(year)) > -1]
+        avgPrice = yearTmp['price'].mean()
+        data.append([town,year,avgPrice])
+        
 print("There are altogether {} rows of data in this dataset.".format(len(d)))
+for x in data: 
+    print("The median resale flat price for {} in {} is ${:.0f}".format(x[0], x[1], x[2]))
